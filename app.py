1b09ce2a1a616bb5588d9ebad17e2122d3419149
Fari <nicolasfarizano10@gmail.com>
2023-06-13 17:03:27 -0300

    commit fari 2, arreglo de variables

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask
-var = "Hola Minoli";
-var = "Hola Minoli";
-var = "Hola Minoli";
 var = "Hola Minoli"
-var2 = var
-var3 = var2
-var4 = var3
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    return var4
+    return var
 
 if __name__ == "__main__":
     app.run()
