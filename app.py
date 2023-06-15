b992085a647448d817b94421cd09e77e3339aa62
Axel <axel_ahe_@hotmail.com>
2023-06-15 16:32:58 -0300

    Parcial

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
 var = "Hola Mundo";
-var2 = "Hola Mundo";
-var3 = "Hola Mundo";
-var4 = "Hola Mundo";
-var5 = "Hola Mundo"
-var2= var
-var3= var2
-var3= var4
-var4= var5
+var = "Hola Mundo";
+var = "Hola Mundo";
+var = "Hola Mundo";
 
 app = Flask(__name__)
 
@@ -18,3 +13,6 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+def index2():
+    return
