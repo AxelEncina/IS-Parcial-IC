bc8bed793cfd35390811234fa3ea265094205a0f
Antonella Perez <antonellapereze19@gmail.com>
2023-06-15 14:41:29 -0300

    testcorrecto

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from flask import Flask
-<<<<<<< HEAD
-var = "Hola Minoli"
+var = "Hola Mundo"
 
-=======
-var = "Hola Mundo";
-var = "Hola Mundo";
-var = "Hola Mundo";
-var2 = var
-var3 = var2
-var4 = var3
->>>>>>> d8b63294e1676e509ccad26478e22bd019837560
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
-    return var4
+    return var
 
 if __name__ == "__main__":
     app.run()
-
-def index2():
-    return
