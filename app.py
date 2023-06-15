c0755a9f1577086bc67ca1c81d8b956aa2f87c26
Antonella Perez <antonellapereze19@gmail.com>
2023-06-15 11:10:33 -0300

    testCodeSmells

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
-var = "Hola Minoli!"
+var = "Hola Minoli";
+var2 = "Hola Minoli";
+var3 = "Hola Minoli";
+var4 = "Hola Minoli";
+var5 = "Hola Minoli"
+var2= var
+var3= var2
+var3= var4
+var4= var5
+
 app = Flask(__name__)
 
 @app.route("/")
