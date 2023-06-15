841af107213b39673c79f1c93563fdbda94bc84b
Antonella Perez <antonellapereze19@gmail.com>
2023-06-15 15:32:16 -0300

    testULTIMOOK

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
-var = "Hola Mundo"
+var = "Hola Mundo";
+var2 = "Hola Mundo";
+var3 = "Hola Mundo";
+var4 = "Hola Mundo";
+var5 = "Hola Mundo"
+var2= var
+var3= var2
+var3= var4
+var4= var5
 
 app = Flask(__name__)
 
